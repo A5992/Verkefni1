a27c3c0d464b11766beaa71335ca8f9356b12e69
A5992 <arnar.einars@gmail.com>
2023-02-24 22:58:55 +0000

    klárt

---
+++ bin/untitled1.dart
@@ -0,0 +1,31 @@
+
+void main() {
+  String name = "Arnar";
+  print('Halló $name');
+
+  int t1 = 23;
+  int t2 = 77;
+  int sum = t1+t2;
+  print("Summan af $t1 og $t2 er $sum");
+
+  double teljari = 36;
+  double nefnari = 4;
+  double svar = teljari/nefnari;
+  print('$teljari deilt með $nefnari er $svar');
+
+  int svar1 = -1+5*5;
+  print('-1+5*5 er $svar1');
+
+  int svar2 = (25+5)%4;
+  print('(25+5)%4 er $svar2');
+  
+  double svar3 = 8+6*4/2;
+  int svarx = svar3.toInt();
+  print('8+6*4/2 er $svarx');
+
+  double svar4 = 2+15/6*1-7%2;
+  print('2+15/6*1-7%2 er $svar4');
+
+
+
+}
